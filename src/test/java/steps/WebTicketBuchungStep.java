c6e8034260027e78324ad002d9f4890bb6972c7c
davidwalser <walser.dave@gmail.com>
2021-06-28 16:05:47 +0200

    Moved Webtests to own file

---
+++ src/test/java/steps/WebTicketBuchungStep.java
@@ -0,0 +1,56 @@
+package steps;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import oebbelements.XPath;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import utility.WebHook;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class WebTicketBuchungStep {
+
+    private static final String MAP_FROM_KEY = "Von";
+    private static final String MAP_TO_KEY = "Nach";
+
+    private WebDriver webDriver;
+
+
+    @Given("Website")
+    public void seleniumWebsite() throws Exception {
+        this.webDriver = WebHook.getDriver();
+        this.webDriver.get("https://tickets.oebb.at/de/ticket/travel");
+    }
+
+    @When("Web choose location")
+    public void chooselocation(DataTable table) throws Exception {
+        List<Map<String, String>> rows = table.asMaps(String.class, String.class);
+        Map<String, String> columns = rows.get(0);
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        var fromElem = this.webDriver.findElement(By.cssSelector("input[name='stationFrom']"));
+        fromElem.click();
+        fromElem.sendKeys(columns.get(MAP_FROM_KEY));
+        fromElem.sendKeys(Keys.ENTER);
+        var toElem = this.webDriver.findElement(By.cssSelector("input[name='stationTo']"));
+        toElem.click();
+        toElem.sendKeys(columns.get(MAP_TO_KEY));
+        webDriver.findElement(By.xpath(XPath.WEB_FIRST_DESTINATION_OPTION)).click();
+
+        webDriver.findElement(By.xpath(XPath.WEB_TICKET_SHOW_ALL)).click();
+
+    }
+
+    @Then("Web Ticket costs")
+    public void ticketcosts() throws Exception {
+        var result = webDriver.findElements(By.xpath(XPath.WEB_TICKET_LIST_ITEM));
+        Assert.assertNotNull("Erwartet, dass mindestens ein ergebniss gefunden wird.", result);
+    }
+
+}
